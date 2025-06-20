07f4143a21b7b3a4c066b4aee0bb9ff9685ce973
guilhermegouw <guilherme.gouw@gmail.com>
2025-06-20 06:31:40 -0300

    Tailwind setup

--- core/settings.py
+++ core/settings.py
@@ -12,6 +12,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # 3rd party apps
+    "django_browser_reload",
+    # Local apps
     "party",
 ]
 
@@ -23,6 +26,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "django_browser_reload.middleware.BrowserReloadMiddleware",
 ]
 
 ROOT_URLCONF = "core.urls"
